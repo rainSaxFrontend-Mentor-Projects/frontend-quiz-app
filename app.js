4894796e3f631f73dd7f7198010df24f676073b6
rainSax <40616002+rainSax@users.noreply.github.com>
2023-11-12 21:54:13 -0800

    add some js for start of question screen

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 
+// fetch returns a Promise, .json() returns a *2nd* Promise, therefore 2 .thens
+fetch('./data.json')
+    .then(response => response.json())
+    .then(data => console.log(data))
 
 // light dark switch section
 const toggleSwitch = document.querySelector('.light-dark-switch input[type="checkbox"]');
@@ -13,3 +17,32 @@ function switchMode(event) {
 }
 
 toggleSwitch.addEventListener('change', switchMode, false);
+
+var buttons = document.querySelectorAll("button");
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
+        var quizType = this.id;
+        questionScreen(quizType);
+    })
+}
+
+function questionScreen(type) {
+    document.querySelector(".start-menu").style.display = "none"
+    document.querySelector(".subject-chosen").innerHTML = type
+    document.querySelector(".curr-subject").style.visibility = "visible"
+
+    // if statement for setting image
+    if (type == "HTML") {
+        document.querySelector(".subject-img").src = "./assets/images/icon-html.svg"
+    }
+    else if (type == "CSS") {
+        document.querySelector(".subject-img").src = "./assets/images/icon-css.svg"
+    }
+    else if (type == "Javascript") {
+        document.querySelector(".subject-img").src = "./assets/images/icon-js.svg"
+    }
+    else {
+        document.querySelector(".subject-img").src = "./assets/images/icon-accessibility.svg"
+    }
+}
